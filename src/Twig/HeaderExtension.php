b1a1d22ceedc23d27213dc434c3b6454bed093cc
ra0ued <ra0ued@zabtech.ru>
2025-05-25 20:08:31 +0300

    Header parsing extension

--- src/Twig/HeaderExtension.php
+++ src/Twig/HeaderExtension.php
@@ -16,14 +16,17 @@ class HeaderExtension extends AbstractExtension
 
     public function parseHeader(string $header): string
     {
-        $pattern = '/Message from HF Pager Gate \(from (\d+) via (\d+)\)/i';
+        $pattern = '/Message from HF Pager Gate \(from (\d+) via (\d+)(?:\/(\d+))?\)/i';
 
         $result = preg_replace_callback($pattern, function ($matches) {
             $id1 = $matches[1];
             $id2 = $matches[2];
+            $id3 = $matches[3] ?? '';
             $link = sprintf('<a href="/%s" target="_blank">%s</a>', $id1, $id1);
 
-            return sprintf('%s через %s', $link, $id2);
+            $viaPart = $id3 ? "$id2/$id3" : $id2;
+
+            return sprintf('%s через %s', $link, $viaPart);
         }, $header);
 
         return $result ?? $header;
